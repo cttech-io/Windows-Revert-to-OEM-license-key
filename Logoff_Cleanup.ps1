356c03a6e353c19521b182730ebb01e81c3f7909
cttech.io <91614698+cttech-io@users.noreply.github.com>
2023-03-18 12:10:32 +0000

    Add files via upload

---
+++ Logoff_Cleanup.ps1
@@ -0,0 +1,14 @@
+﻿#Clear User Downloads Folder of antything older than 7 days old
+Get-ChildItem –Path $env:UserProfile\Downloads\* -Recurse | Where-Object { $_.LastWriteTime –lt (Get-Date).AddDays(-7) } | Remove-Item -Force
+
+#Clear Down Teams Cache
+$TeamsCache="$env:AppData\Microsoft\Teams\Service Worker\CacheStorage"
+If (Test-Path $TeamsCache){
+	Remove-Item $TeamsCache\* -Recurse -Force}
+
+#Clear Down User Thunderbird Index Files 
+Get-ChildItem -Path $env:AppData\Thunderbird\Profiles\ *.sbd -Recurse | foreach { Remove-Item -Path $_.FullName -Force -Recurse }
+Get-ChildItem -Path $env:AppData\Thunderbird\Profiles\ *.msf -Recurse | foreach { Remove-Item -Path $_.FullName -Force -Recurse }
+
+#Clear Recycle Bin
+Clear-RecycleBin -DriveLetter C -Force
---
+++ Start_HomeAssistant.bat
@@ -0,0 +1,2 @@
+SLEEP 60
+"C:\Program Files\Oracle\VirtualBox\VboxManage.exe" startvm HomeAssistant --type headless
